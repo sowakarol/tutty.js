d9bcda4b8e46f1f0374397ef1661e6ef4b755240
Filip Prasalek <filipprasalek@gmail.com>
2018-01-06 20:01:16 +0100

    Overlay component * draft update

--- src/app/overlay/overlay.component.ts
+++ src/app/overlay/overlay.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+const TEST_DATA: number[] = [1 , 2, 3, 4, 5, 6, 7, 8, 9];
+
+
 @Component({
   selector: 'app-overlay',
   templateUrl: './overlay.component.html',
@@ -7,15 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OverlayComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
+
+  ngOnInit() {
+    this.currentHintIndex = 0;
+    this.numberOfHints = TEST_DATA.length;
+  }
+
+ // TODO: change on hint type
+  currentHintIndex: number;
+  numberOfHints: number;
 
   overlayDisplay= 'block';
+  nextButtonText= 'Next';
+
+  showNextHint = () => {
+    if (this.currentHintIndex === this.numberOfHints - 1) {
+      this.close();
+      return;
+    }
+
+    this.currentHintIndex++;
+    if (this.currentHintIndex === this.numberOfHints - 1) {
+      this.nextButtonText = 'Close';
+    }
+
+  }
+
+  showPrevHint = () => {
+    this.currentHintIndex--;
+    if (this.currentHintIndex === this.numberOfHints - 2) {
+      this.nextButtonText = 'Next';
+    }
+  }
 
   close = () => {
     this.overlayDisplay = 'none';
-  }
-
-  ngOnInit() {
   }
 
 }
