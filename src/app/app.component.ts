bc0645de5e2ff997df8ca968951a4e11007ecdf6
Epy <epyepyepy@gmail.com>
2018-01-27 02:31:57 +0100

    property bind in overlay component for poper hint set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,30 +4,22 @@ import {ChatExampleData} from './data/chat-example-data';
 import {UsersService} from './user/users.service';
 import {ThreadsService} from './thread/threads.service';
 import {MessagesService} from './message/messages.service';
-import {JsonParserService} from './parser/json-parser.service';
 import { OnInit, AfterViewInit, AfterViewChecked } from '@angular/core/src/metadata/lifecycle_hooks';
-import { HintCollection, Hint } from './util/classes';
-import { CookiesHandlerService } from './cookies-handler/cookies-handler.service';
 import { OverlayComponent } from './overlay/overlay.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [JsonParserService],
   entryComponents: [OverlayComponent]
 })
 export class AppComponent {
   constructor(public messagesService: MessagesService,
               public threadsService: ThreadsService,
               public usersService: UsersService,
-              public parser: JsonParserService,
-              private cookiesHandler: CookiesHandlerService,
-              private resolver: ComponentFactoryResolver,
               private cdRef: ChangeDetectorRef
               ) {
     ChatExampleData.init(messagesService, threadsService, usersService);
-    console.log(parser.parse());
   }
 
 }
--- src/app/overlay/overlay.component.ts
+++ src/app/overlay/overlay.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, AfterViewChecked, AfterViewInit, ViewChild, ViewContainerRef, ComponentFactory,
-  ComponentFactoryResolver, ChangeDetectorRef} from '@angular/core';
+  ComponentFactoryResolver, ChangeDetectorRef, Input} from '@angular/core';
 import { PopupService } from '../popup/popup.service';
 import { PopupComponent } from '../popup/popup.component';
 import { HintProviderService } from '../hint-provider/hint-provider.service';
 import { Hint } from '../util/classes';
+import { JsonParserService } from '../parser/json-parser.service';
 
 const TEST_DATA: number[] = [0, 1 , 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -12,9 +13,13 @@ const TEST_DATA: number[] = [0, 1 , 2, 3, 4, 5, 6, 7, 8, 9];
   templateUrl: './overlay.component.html',
   styleUrls: ['./overlay.component.css'],
   entryComponents: [PopupComponent],
-  providers: [HintProviderService, PopupService]
+  providers: [JsonParserService, 
+    HintProviderService, 
+    PopupService]
 })
 export class OverlayComponent implements OnInit, AfterViewInit, AfterViewChecked {
+
+  @Input() collection: string;
 
   @ViewChild(
     'popupContainer', {
@@ -28,12 +33,11 @@ export class OverlayComponent implements OnInit, AfterViewInit, AfterViewChecked
     public popupService: PopupService,
     private resolver: ComponentFactoryResolver,
     private cdRef: ChangeDetectorRef,
-    private hintsService: HintProviderService) {
-      this.hints = hintsService.getHints("first");
-      this.popupService.setHints(this.hints);
-    }
+    private hintsService: HintProviderService) { }
 
   ngOnInit() {
+    this.hints = this.hintsService.getHints(this.collection);
+    this.popupService.setHints(this.hints);
     this.currentHintIndex = 0;
     this.numberOfHints = this.hints.length;
   }
