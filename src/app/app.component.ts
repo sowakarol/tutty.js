cee8f055cfb755bd4b3b883febd3749aa0d07930
Epy <epyepyepy@gmail.com>
2018-02-11 02:30:16 +0100

    another site to test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { OverlayComponent } from './modules/tutty/overlay/overlay.component';
+import { AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild(OverlayComponent) tutty: OverlayComponent;
+
+
   title = 'app';
+
+  ngAfterViewInit(): void {
+    this.tutty.show("first");
+  }
+
 }
