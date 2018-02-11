235491822a8d00050a567ae312d6aae62a653333
sowakarol <karosowa@student.agh.edu.pl>
2018-02-11 23:43:14 +0100

    margin and positioning fix

--- src/app/popup/popup-strategy-impl.ts
+++ src/app/popup/popup-strategy-impl.ts
@@ -4,9 +4,10 @@ export class PopupStrategyTop extends PopupStrategyInterface {
 
   setPosition(boundingClientRect, popupComponent) {
     this.positionPopup(boundingClientRect);
-    popupComponent.popupTop = this.top - this.height - 2 * 25 + 'px' + '';
+    popupComponent.popupTop = this.top - this.height - 20  +  'px'; //margin arrow
+    console.log(this);
     popupComponent.popupBottom = '';
-    popupComponent.popupLeft = - (this.width / 2) + ((this.left + this.right)/2) + 'px';
+    popupComponent.popupLeft =  (this.width / 2) - this.width + this.left + 12   + 'px'; //margin
     this.positionArrow(document.getElementById('arrow'),'arrow-down');
   };
 }
@@ -15,10 +16,12 @@ export class PopupStrategyBottom extends PopupStrategyInterface {
 
   setPosition(boundingClientRect, popupComponent) {
     this.positionPopup(boundingClientRect);
-    popupComponent.popupTop = this.bottom + 'px';
+    popupComponent.popupTop = this.bottom  + 6  +  'px'; //arrow
     popupComponent.popupBottom = '';
-    console.log(this.width);
-    popupComponent.popupLeft = - (this.width / 2) + ((this.left + this.right)/2)+ 'px';
+    console.log(this);
+    popupComponent.popupLeft =  - (this.width / 2) + ((this.left + this.right)/2)+ 'px';
+    //popupComponent.popupLeft =  (this.width / 2) - this.width + this.left + 12   + 'px'; //margin
+
     console.log(popupComponent.popupLeft );
     this.positionArrow(document.getElementById('arrow'),'arrow-up');
   };
@@ -29,7 +32,7 @@ export class PopupStrategyLeft extends PopupStrategyInterface {
   setPosition(boundingClientRect, popupComponent) {
     this.positionPopup(boundingClientRect);
     popupComponent.popupBottom = '';
-    popupComponent.popupTop = this.top - this.bottom + 'px';
+    popupComponent.popupTop = this.top + ((this.top - this.bottom) / 2) - 6  + 'px';
     popupComponent.popupRight = this.innerWindowWidth - this.left + 'px';
     popupComponent.popupLeft = 'initial';
     this.positionArrow(document.getElementById('arrow'),'arrow-right');
@@ -42,11 +45,12 @@ export class PopupStrategyRight extends PopupStrategyInterface {
   setPosition(boundingClientRect, popupComponent) {
     this.positionPopup(boundingClientRect);
     popupComponent.popupBottom = '';
-    popupComponent.popupTop = this.top + ((this.top - this.bottom) / 2)  + 'px';
+    popupComponent.popupTop = this.top + ((this.top - this.bottom) / 2) + 6  + 'px';
     console.log(this);
-    popupComponent.popupRight = this.innerWindowWidth - this.right - this.left - this.width + 'px';
+    popupComponent.popupRight = this.innerWindowWidth - this.right + this.left + 'px';
     console.log(popupComponent.popupRight);
-    popupComponent.popupLeft = 'initial';
+    popupComponent.popupLeft = this.right + 10 + 'px';
+    popupComponent.popupRight = 'initial';
 
     this.positionArrow(document.getElementById('arrow'),'arrow-left');
   };
