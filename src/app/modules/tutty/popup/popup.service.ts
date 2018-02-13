f45731f0942328363e3d407487211682e2184116
Siorek, Oskar <oskar.siorek@sabre.com>
2018-02-13 21:34:41 +0100

    fix zindex not working for elements other than divs

--- src/app/modules/tutty/popup/popup.service.ts
+++ src/app/modules/tutty/popup/popup.service.ts
@@ -80,9 +80,11 @@ export class DivVO {
   }
 
   public enlightenReference(divRef) {
+    if(divRef.tagName != 'DIV'){
+      divRef.style.position = 'relative';
+    }
     // divRef.style.boxShadow = '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)';
     divRef.style.zIndex = 2141;
-    divRef.style.position = 'relative';
     divRef.style.pointerEvents = 'none';
   }
 }
