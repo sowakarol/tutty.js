e79418c766d342517c918c9df7f1c64223e87f6c
Epy <epyepyepy@gmail.com>
2018-02-14 00:27:16 +0100

    export typo

--- src/public_api.ts
+++ src/public_api.ts
@@ -3,6 +3,6 @@ export * from './app/modules/tutty/overlay/overlay.component';
 export * from "./app/modules/tutty/popup/popup.component";
 export * from './app/modules/tutty/cookies-handler/cookies-adapter.service';
 export * from './app/modules/tutty/paging/paging.component';
-export * form './app/modules/tuttty/tutty/tutty.service';
+export * from './app/modules/tuttty/tutty/tutty.service';
 
 export * from './app/modules/tutty/tutty.module';
