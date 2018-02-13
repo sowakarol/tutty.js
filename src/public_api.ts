85c71a212861b19e8bbea0ece21b1d671781b5a8
sowakarol <karosowa@student.agh.edu.pl>
2018-02-13 20:53:06 +0100

    Fixed exports and update version

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,1 +1,8 @@
+export * from "./app/modules/tutty/util/config"
+export * from './app/modules/tutty/overlay/overlay.component';
+export * from "./app/modules/tutty/popup/popup.component";
+export * from './app/modules/tutty/cookie-test/cookie-test.component';
+export * from './app/modules/tutty/cookies-handler/cookies-handler.service';
+export * from './app/modules/tutty/paging/paging.component';
+
 export * from './app/modules/tutty/tutty.module'
