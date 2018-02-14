db25a9155ae2dca38e15ce9e9d2e915ad0ab08f6
Siorek, Oskar <oskar.siorek@sabre.com>
2018-02-14 02:33:24 +0100

    upel site hinted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,20 @@
-import { Component, ViewChild } from '@angular/core';
-import { OverlayComponent } from './modules/tutty/overlay/overlay.component';
-import { AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import {Component} from '@angular/core';
+import {AfterViewInit} from '@angular/core/src/metadata/lifecycle_hooks';
 import {TuttyService} from "./modules/tutty/tutty/tutty.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // templateUrl: './plik2.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements AfterViewInit {
 
-  constructor(private tutty: TuttyService) { }
-  title = 'app';
+  constructor(private tutty: TuttyService) {
+  }
 
   ngAfterViewInit(): void {
     this.tutty.displayHints("first", 'inner');
+    // this.tutty.displayHints("upel", 'upelek');
   }
-
 }
